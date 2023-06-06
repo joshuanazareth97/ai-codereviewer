05ab00959fbc28f731a8290a6d61ab52fd1bc4cb
Joshua <joshuanazareth97@gmail.com>
2023-06-07 01:24:03 +0530

    Placeholder for diff parse

--- src/diffParse.ts
+++ src/diffParse.ts
@@ -1,30 +1,30 @@
-import gitDiffParser from "gitdiff-parser";
+// import gitDiffParser from "gitdiff-parser";
 
-let gitDiffText = "";
+// let gitDiffText = "";
 
-process.stdin.on("readable", () => {
-  let chunk;
-  while (null !== (chunk = process.stdin.read())) {
-    gitDiffText += chunk;
-  }
-});
+// process.stdin.on("readable", () => {
+//   let chunk;
+//   while (null !== (chunk = process.stdin.read())) {
+//     gitDiffText += chunk;
+//   }
+// });
 
-function parseDiff() {
-  const files = gitDiffParser.parse(gitDiffText);
+// function parseDiff() {
+//   const files = gitDiffParser.parse(gitDiffText);
 
-  files.forEach((file) => {
-    console.log(`+++++++++++ ${file.newPath} ++++++++++++`);
-    file.hunks.forEach((hunk) => {
-      let block = "";
-      block += hunk.changes
-        .filter((change) => change.type === "insert")
-        .map((change) => change.content)
-        .join("\n");
-      console.log(block);
-      console.log("");
-    });
-    console.log("\n");
-  });
-}
+//   files.forEach((file) => {
+//     console.log(`+++++++++++ ${file.newPath} ++++++++++++`);
+//     file.hunks.forEach((hunk) => {
+//       let block = "";
+//       block += hunk.changes
+//         .filter((change) => change.type === "insert")
+//         .map((change) => change.content)
+//         .join("\n");
+//       console.log(block);
+//       console.log("");
+//     });
+//     console.log("\n");
+//   });
+// }
 
-process.stdin.on("end", parseDiff);
+// process.stdin.on("end", parseDiff);
